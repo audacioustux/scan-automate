2dd2c58dc640f2d69cab5d84a81f4d6c0d2237fc
Audacious Tux <tangimhossain1@gmail.com>
2023-10-27 19:47:28 +0000

    add basic api-server, bin/k8s-provision.sh

---
+++ scan-automate/api/src/main.rs
@@ -0,0 +1,59 @@
+use std::net::SocketAddr;
+
+use axum::{
+    http::{HeaderValue, Method},
+    response::{Html, IntoResponse},
+    routing::get,
+    Json, Router,
+};
+use listenfd::ListenFd;
+use tokio::net::TcpListener;
+use tower_http::cors::CorsLayer;
+
+#[tokio::main]
+async fn main() {
+    let frontend = async {
+        let app = Router::new().route("/", get(html));
+        serve(app, 3000).await;
+    };
+
+    let backend = async {
+        let app = Router::new().route("/json", get(json)).layer(
+            CorsLayer::new()
+                .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
+                .allow_headers(vec![http::header::CONTENT_TYPE])
+                .allow_methods([Method::GET]),
+        );
+        serve(app, 4000).await;
+    };
+
+    tokio::join!(frontend, backend);
+}
+
+async fn serve(app: Router, port: u16) {
+    let addr = SocketAddr::from(([127, 0, 0, 1], port));
+
+    let mut listenfd = ListenFd::from_env();
+    let listener = match listenfd.take_tcp_listener(0).unwrap() {
+        Some(listener) => TcpListener::from_std(listener).unwrap(),
+        None => TcpListener::bind("[::]:3000").await.unwrap(),
+    };
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn html() -> impl IntoResponse {
+    Html(
+        r#"
+        <script>
+            fetch('http://localhost:4000/json')
+              .then(response => response.json())
+              .then(data => console.log(data));
+        </script>
+        "#,
+    )
+}
+
+async fn json() -> impl IntoResponse {
+    Json(vec!["one", "two", "three"])
+}
--- scan-automate/bin/k8s-provision.sh
+++ scan-automate/bin/k8s-provision.sh
@@ -2,4 +2,55 @@
 
 set -ea
 
+export REPO=`git remote get-url origin`
+export ARGOCD_OPTS='--port-forward --port-forward-namespace argocd'
 
+deploy-argocd(){
+    echo "Deploying argocd..."
+    kubectl create namespace argocd --dry-run=client -o yaml | kubectl apply -f -
+    kubectl apply -k k8s/kustomize/argocd -n argocd
+
+    echo "Waiting for all argocd pods to be ready..."
+    kubectl wait --for=condition=ready pod \
+        --all \
+        -n argocd \
+        --timeout=300s
+}
+
+login-argocd(){
+    echo "Logging in to argocd..."
+    local password=`kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d`
+
+    argocd login --username admin --password $password
+}
+
+add-private-repos(){
+    echo "Adding private repo..."
+    local repo_url=($REPO)
+
+    for repo in "${repo_url[@]}"
+    do
+        argocd repo add $repo --username ${GIT_USERNAME:?} --password ${GIT_TOKEN:?}
+    done
+}
+
+deploy-argocd-apps(){
+    echo "Deploying argocd apps..."
+    kubectl apply --recursive -f k8s/apps
+}
+
+deploy-secrets(){
+    echo "Deploying secrets..."
+    local git=`kubectl create secret generic git-config --from-literal=username=$GIT_USERNAME --from-literal=password=$GIT_TOKEN --dry-run=client -o yaml`
+    local docker=`kubectl create secret generic docker-config --from-file=$HOME/.docker/config.json --dry-run=client -o yaml`
+
+    echo "$git" | kubectl apply -n argo -f -
+    echo "$docker" | kubectl apply -n argo -f -
+}
+
+deploy-argocd
+login-argocd
+add-private-repos
+deploy-argocd-apps
+
+ebort -- deploy-secrets 2> /dev/null
